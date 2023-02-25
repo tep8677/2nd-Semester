a80bc9b93e4fe748c8fe8b027cadea8099396076
deadly-unicorn <linatic@pm.me>
2023-02-25 04:04:44 +0200

    askhsh ypologismoy

--- Python/25-2/ypologismos_forou.py
+++ Python/25-2/ypologismos_forou.py
@@ -1,6 +1,8 @@
-misthos=800;
-klimakaForou=0.5;
+misthos=800; #mhniaios misthos
+klimakaForou=0.5; #50% = 0.5
 
 ethsiosForos=misthos*klimakaForou*12;
 
 print("o ethsios foros einai:",ethsiosForos);
+
+
