6d5035184313170e93187536b6c037496f062d0f
deadly-unicorn <linatic@pm.me>
2023-02-27 20:12:08 +0200

    python basics

---
+++ Python/21-2/basics1.py
@@ -0,0 +1,12 @@
+## comments
+
+bools=[True,False];
+print(9//5); #=>1 (int division())
+print(type('a'),type(True)); #=> types are classes in python
+print(type(bools))
+
+print (True and False);
+print (True or False);
+print (True and not False);
+
+print('hello world!',bools)
