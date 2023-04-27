36277927161e2579b18ae8b6ecf7cd5dca144167
deadly-unicorn <retroalex1008@gmail.com>
2023-04-27 14:39:20 +0300

    back from easter

---
+++ Python/25-04-2023/hello_world.py
@@ -0,0 +1,11 @@
+i=0
+while True:
+    print (i+1,": hello world")
+    i=i+1
+    if (i==10):
+        break
+print("hello world {}".format(i))
+
+# we actually started learning 
+# about using python on web apps
+# that day.
